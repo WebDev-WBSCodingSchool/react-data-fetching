9f44f958919cb05a0f84fccc94b2ca01cc97f7fd
Jorge Paul <jserranopaul@gmail.com>
2025-06-03 19:11:56 +0200

    Render as you fetch

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export { default as FetchOnRenderComments } from './FetchOnRenderComments.jsx';
 export { default as Loading } from './Loading.jsx';
 export { default as Instructions } from './Instructions.jsx';
+export { default as RenderAsYouFetchComments } from './RenderAsYouFetchComments.jsx';
+export { default as RenderAsYouFetchPost } from './RenderAsYouFetchPost.jsx';
--- src/pages/RenderAsYouFetchOrchestrator.jsx
+++ src/pages/RenderAsYouFetchOrchestrator.jsx
@@ -1,5 +1,28 @@
+import { Suspense, useRef } from 'react';
+import { useParams } from 'react-router';
+import { Loading, RenderAsYouFetchComments, RenderAsYouFetchPost } from '../components';
+import { getPostById, getCommentsByPostId } from '../utils';
+
 const RenderAsYouFetchOrchestrator = () => {
-  return <div>RenderAsYouFetchOrchestrator</div>;
+  const { id } = useParams();
+  const cache = useRef(new Map());
+
+  if (!cache.current.get(id)) {
+    const post = getPostById(id);
+    const comments = getCommentsByPostId(id);
+    cache.current.set(id, { post, comments });
+  }
+
+  return (
+    <>
+      <Suspense fallback={<Loading message={`Loading post ${id}`} />}>
+        <RenderAsYouFetchPost promise={cache.current.get(id).post} />
+      </Suspense>
+      <Suspense fallback={<Loading message={`Loading comments for post ${id}`} />}>
+        <RenderAsYouFetchComments promise={cache.current.get(id).comments} />
+      </Suspense>
+    </>
+  );
 };
 
 export default RenderAsYouFetchOrchestrator;
